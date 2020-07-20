5df4feed97af4db514be8da2b53b6bc9794883d1
oskr814 <oskralvarez814@gmail.com>
2020-07-20 16:49:37 -0600

    [ADD] Excercise AdvViews: Calendar

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from odoo import models, fields, api
 
 
@@ -9,6 +10,8 @@ class Session(models.Model):
     # start_date = fields.Date()
     # Establecer un valor por defecto
     start_date = fields.Date(default=fields.Date.today)
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
     active = fields.Boolean(default=True)
 
     duration = fields.Float(digits=(6, 2), help="Duration in days")
@@ -47,6 +50,27 @@ class Session(models.Model):
                 },
             }
     
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            r.duration = (r.end_date - r.start_date).days + 1
+
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for r in self:
